f572a696ab82d46f4f940da5a0ba81d0b9d65efb
David Permsky <dpthierry@gmail.com>
2022-07-01 13:16:35 -0500

    pets

---
+++ src/gestalt/retrof.jl
@@ -0,0 +1,24 @@
+# vim: tabstop=2 shiftwidth=2 expandtab colorcolumn=80
+#############################################################################
+#  Copyright 2022, David Thierry, and contributors
+#  This Source Code Form is subject to the terms of the MIT
+#  Liosense.
+#############################################################################
+
+struct retrofForm
+  # capital cost
+  rCap::Function
+  # variable cost
+  rVar::Function
+  # fixed cost
+  rFix::Function
+  # heat rate
+  rHtr::Function
+  # carbon factor
+  rCo::Function
+  # fuel cost
+  rFuel::Function
+end
+
+
+
