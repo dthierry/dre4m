f62542142d882a4481dd278a522ded763c585a12
dthierry <dpthierry@gmail.com>
2022-08-10 13:24:46 -0500

    lolE

--- data/egridproc.jl
+++ data/egridproc.jl
@@ -53,9 +53,6 @@ for row in eachrow(dfu)
     # unit id
     uid = string(row["UNITID"]) # always string
     aka = getAka(uid)
-    #print(typeof(aka))
-    #print(typeof(uid))
-    #print("\n")
     t_aka = Tuple{String, String}((aka, uid))
     # unit operational status
     # sometimes these are "missing"
@@ -160,7 +157,6 @@ for row in eachrow(dfg)
                 if akaG == k[1] # found
                     genidG = k[2]
                     dl = d[genidG] # attempt to use this key entry instead
-                    println(dl)
                     if isa(dl[3], Missing)
                         break
                     end
@@ -228,7 +224,11 @@ opdf = filter(row -> row.GENSTAT == "OP", dfg)
 dropmissing!(opdf, :SECTOR)
 
 # filter out only electric utilities
-filter!(row -> row.SECTOR == "Electric Utility", opdf)
+filter!(row -> 
+        row.SECTOR == "Electric Utility" ||
+        row.SECTOR == "IPP CHP" ||
+        row.SECTOR == "IPP Non-CHP", 
+        opdf)
 
 minyr = minimum(opdf[:, :GENYRONL])
 maxyr = minimum(opdf[:, :GENYRONL])
@@ -250,18 +250,18 @@ for df in grpf
 end
 
 # sort by year
-sort!(dfCap, :GENYRONL)
+sort!(dfCap, :GENYRONL, rev=true)
 # make the years into strings
 transform!(dfCap, [:GENYRONL] => (x->string.(x)) => :GENYRONL)
 # transpose
 dfCap = permutedims(dfCap, 1)
-XLSX.writetable("cap2.xlsx", dfCap)
+#XLSX.writetable("cap2.xlsx", dfCap)
 
 # heat rates
 # drop the rows without hr
-dropmissing!(opdf, :HR)
+opdf2 = dropmissing(opdf, :HR)
 # group by prime-mover-fuel
-grpf = groupby(opdf, :PMPF)
+grpf = groupby(opdf2, :PMPF)
 # create weighted average hr dataframe
 dfWavHr = DataFrame(:GENYRONL=>minyr:maxyr)
 for df in grpf
@@ -281,26 +281,159 @@ for df in grpf
 end
 
 # sort by year
-sort!(dfWavHr, :GENYRONL)
+sort!(dfWavHr, :GENYRONL, rev=true)
 
 transform!(dfWavHr, :GENYRONL => (x->string.(x))=> :GENYRONL)
 
 # transpose
 dfWavHr = permutedims(dfWavHr, 1)
 
-XLSX.writetable("hr2.xlsx", dfWavHr)
+#XLSX.writetable("hr2.xlsx", dfWavHr)
 
+modelTech = ["PC", "CT", "CC", "P", "B", "N", "H", "W",  "SPV", "STH", "G"]
+techFuel = Dict("PC"=>["BIT", "LIG", "SUB", "RC", "SGC", "COG"],
+                "CT"=>["NG"],
+                "CC"=>["NG"],
+                "P"=>["DFO", "JF", "KER", "PC", "RG", "RFO", "WO"],
+                "B"=>["OBG", "OBL", "OBS"],
+                "N"=>["NUC"],
+                "H"=>["WAT"],
+                "W"=>["WND"],
+                "SPV"=>["SUN"],
+                "STH"=>["SUN"],
+                "G"=>["GEO"])
+techMover = Dict("PC"=>[], # empty means all
+                "CT"=>["GT", "IC"],
+                "CC"=>["CA", "CC", "CT", "ST"],
+                "P"=>[],
+                "B"=>[],
+                "N"=>[],
+                "H"=>["HY"],
+                "W"=>["WT"],
+                "SPV"=>["PV"],
+                "STH"=>["CP", "OT", "ST"],
+                "G"=>[])
 
-# BIT, LIG, SUB, RC, WC, SGC, COG => COAL
-# NG, BU => NG
+# capacities
+grpf = groupby(opdf, :FUELG1)
+dC = Dict([i=>DataFrame() for i in modelTech])
+
+for mT in modelTech
+    d0 = dC[mT]
+    for fG in techFuel[mT]
+        fGd = get(grpf, (FUELG1=fG,), nothing)
+        if isa(fGd, Nothing)
+            @info "$(mT): fuel=$(fG) does not exist."
+            continue
+        end
+        movers = techMover[mT]
+        if length(movers) > 0
+            for m in movers
+                mGd = filter(:PRMVR=> x->x==m, fGd)
+                append!(d0, mGd)
+            end
+        else
+            append!(d0, fGd)
+        end
+    end
+end
+
+dfCap2 = DataFrame(:GENYRONL=>minyr:maxyr)
+for mT in modelTech
+    df = dC[mT]
+    if size(df)[1] == 0
+        continue
+    end
+    gname = mT
+    yrHr = combine(groupby(df, :GENYRONL),
+                   :NAMEPCAP => sum 
+                   => Symbol(gname))
+    global dfCap2 = outerjoin(dfCap2, yrHr, on=:GENYRONL)
+end
+# sort by year
+sort!(dfCap2, :GENYRONL, rev=true)
+transform!(dfCap2, :GENYRONL => (x->string.(x))=> :GENYRONL)
+# transpose
+dfCap2 = permutedims(dfCap2, 1)
+XLSX.writetable("cap3.xlsx", dfCap2)
+
+# heat rates
+grpf = groupby(opdf2, :FUELG1)
+dTech = Dict([i=>DataFrame() for i in modelTech])
+
+for mT in modelTech
+    d0 = dTech[mT]
+    for fG in techFuel[mT]
+        fGd = get(grpf, (FUELG1=fG,), nothing)
+        if isa(fGd, Nothing)
+            @info "$(mT): fuel=$(fG) does not exist."
+            continue
+        end
+        movers = techMover[mT]
+        if length(movers) > 0
+            for m in movers
+                mGd = filter(:PRMVR=> x->x==m, fGd)
+                append!(d0, mGd)
+            end
+        else
+            append!(d0, fGd)
+        end
+    end
+end
+
+dfWavHr2 = DataFrame(:GENYRONL=>minyr:maxyr)
+for mT in modelTech
+    df = dTech[mT]
+    if size(df)[1] == 0
+        continue
+    end
+    gname = mT*"_WA_HR"
+    yrHr = combine(groupby(df, :GENYRONL),
+                   [:GENNTAN, :HR] => ((x,y)->sum((x./sum(x)).*y)) 
+                   => Symbol(gname))
+    global dfWavHr2 = outerjoin(dfWavHr2, yrHr, on=:GENYRONL)
+end
+# sort by year
+sort!(dfWavHr2, :GENYRONL, rev=true)
+transform!(dfWavHr2, :GENYRONL => (x->string.(x))=> :GENYRONL)
+# transpose
+dfWavHr2 = permutedims(dfWavHr2, 1)
+
+XLSX.writetable("hr3.xlsx", dfWavHr2)
+
+# A technology is a combination of mover-fuel
+# "Pulverized Coal PC" techs (with all movers, but typically just ST)
+# BIT, LIG, SUB, RC, WC, SGC, COG 
+
+# "Gas Turbine aka combustion turbine" (mover={IC})
+# NG 
+
+# "Combined Cycle" (mover={GT, IC})
+# NG
+
+# "Petroleum" (mover={CA, CT, GT, IC, ST})
 # DFO, JF, KER, PC, RG, RFO, WO => PETROLEUM
+
 # BFG, OG, TDF => OTHER FOSSIL
+
+# "ALL NUKE" (mover={ST})
 # NUC
-# OBG, OBL
-# SUN
+
+# "Hydroelectric WAT" (mover={HY})
 # WAT
-#
-#
+
+# "B" (mover={IC})
+# OBG, OBL, OBS
+
+# "SPV" (moved={ST})
+# SUN
+
+# "STH" (not existing? mover={ST})
+# SUN
+
+# "Geothermal" mover={ST}
+# GEO
+
 # Pulverized Coal (PC)
 # Natural Gas (NGGT)
 # Natural Gas (NGCC)
